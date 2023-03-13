e540cfa7c9d3ef319189741026a3d063df5cbf3e
Hazem Ibrahim <hazem.moh98@gmail.com>
2023-03-13 14:09:42 +0100

    renamed files

--- main.py
+++ main.py
@@ -1,30 +1,32 @@
-# pip install googletrans==3.1.0a0
 from googletrans import Translator
 
 def rreplace(s, old, new, occurrence):
+
     li = s.rsplit(old, occurrence)
+
     return new.join(li)
 
 if __name__ == "__main__":
+    inputLocale = 'en'
+    outputLocales = ['fr', 'it']
+
     translator = Translator()
+    file = open("input.arb", "r")
 
-    file = open("english.txt", "r")
-
-    supporedLocales = ['fr', 'it']
-    
     translations = {}
     
     for line in file:
+        #Ignore empty lines and brackets
         if(line == '{\n' or line == '}' or line == ''):
             continue
-        
+
         toTranslate = line.split('": ')[1][1:]
-        toTranslate = toTranslate.split('"')[0]
-    
+        toTranslate = toTranslate.split('"')[0]    
+
         print(toTranslate)
     
-        for supporedLocale in supporedLocales:
-            translation = translator.translate(toTranslate, src='en', dest=supporedLocale)
+        for supporedLocale in outputLocales:
+            translation = translator.translate(toTranslate, src=inputLocale, dest=supporedLocale)
             newLine = rreplace(line, toTranslate, translation.text, 1)
     
             if(supporedLocale in translations.keys()):
@@ -39,4 +41,3 @@ if __name__ == "__main__":
                 f.write(line)
             f.write('}')
             f.close()
-    
